5cb8aa6cd92f2795665532502e63a16c34d494cf
Simon Xu <xsqfeather@gmail.com>
2022-02-21 09:48:40 +0800

    ob

--- src/events/DemoEmitter.ts
+++ src/events/DemoEmitter.ts
@@ -3,7 +3,6 @@ import { Service } from "typedi";
 import { DocumentType } from "@typegoose/typegoose";
 import { on_article_created } from "../constants/events";
 import { Article } from "../models/Article";
-import OnEvent from "../decorators/OnEvent";
 
 @Service()
 export class DemoEmitter extends BaseEventEmitter<

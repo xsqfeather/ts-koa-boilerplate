45f3342c2d9bf1d155bc162e1a5993f1a657b9ee
Simon 徐 <simonxu@SimondeMacBook-Pro.local>
2022-04-17 18:04:27 +0800

    多进程写入

--- src/app.ts
+++ src/app.ts
@@ -15,16 +15,17 @@ startApp({
       const insertVodQueue = new JobQueue("insertVodQueue");
       await insertVodQueue.init();
       // eslint-disable-next-line @typescript-eslint/ban-types
-      setInterval(async () => {
-        const task = await insertVodQueue.popMsg();
-        if (task) {
-          const toInserted = JSON.parse(task);
-          const vodResourceService = Container.get(VodResourceService);
-          const vodTypeService = Container.get(VodTypeService);
-          vodTypeService.createOneByName(toInserted.type_name);
-          vodResourceService.createOrUpdate(toInserted);
-        }
-      }, 1500);
     }
+
+    setInterval(async () => {
+      const task = await insertVodQueue.popMsg();
+      if (task) {
+        const toInserted = JSON.parse(task);
+        const vodResourceService = Container.get(VodResourceService);
+        const vodTypeService = Container.get(VodTypeService);
+        vodTypeService.createOneByName(toInserted.type_name);
+        vodResourceService.createOrUpdate(toInserted);
+      }
+    }, 1500);
   },
 });
